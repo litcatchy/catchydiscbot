ba7b22d554fb9aa6eea16a0bcec32b31efba8002
spyromasked <159229360+spyromasked@users.noreply.github.com>
2025-03-07 14:03:41 +0530

    Update roleadd.py

--- cogs/roleadd.py
+++ cogs/roleadd.py
@@ -1,7 +1,7 @@
 from discord.ext import commands
 import discord
 
-class RoleManagement(commands.Cog):
+class RoleAssignment(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
@@ -79,4 +79,4 @@ class RoleManagement(commands.Cog):
         await ctx.send(embed=embed)
 
 async def setup(bot):
-    await bot.add_cog(RoleManagement(bot))
+    await bot.add_cog(RoleAssignment(bot))
