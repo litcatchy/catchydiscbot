48c2a0ae6754652a3ee283fa4d9c5b745dfcd67b
spyromasked <159229360+spyromasked@users.noreply.github.com>
2025-03-07 14:10:15 +0530

    Update roleadd.py

--- cogs/roleadd.py
+++ cogs/roleadd.py
@@ -13,36 +13,23 @@ class RoleAssignment(commands.Cog):
         role = discord.utils.get(guild.roles, name=role_name)
 
         if role is None:
-            embed = discord.Embed(
-                description=f"{ctx.author.mention} <:cancel:1346853536738316339> No role named `{role_name}` exists.",
-                color=discord.Color.red()
-            )
-            embed.set_footer(text="Ensure the role name is spelled correctly.")
+            embed = discord.Embed(description=f"{ctx.author.mention} <:cancel:1346853536738316339> No role named `{role_name}` exists.", color=discord.Color.red())
             await ctx.send(embed=embed)
             return  
 
         if not ctx.guild.me.guild_permissions.manage_roles:
-            embed = discord.Embed(
-                description=f"{ctx.author.mention} <:cancel:1346853536738316339> I do not have permission to **Manage Roles**.",
-                color=discord.Color.red()
-            )
+            embed = discord.Embed(description=f"{ctx.author.mention} <:cancel:1346853536738316339> I do not have permission to **Manage Roles**.", color=discord.Color.red())
             await ctx.send(embed=embed)
             return  
 
         if role >= ctx.guild.me.top_role:
-            embed = discord.Embed(
-                description=f"{ctx.author.mention} <:cancel:1346853536738316339> I cannot assign `{role.name}` because it is higher or equal to my highest role.",
-                color=discord.Color.red()
-            )
+            embed = discord.Embed(description=f"{ctx.author.mention} <:cancel:1346853536738316339> I cannot assign `{role.name}` because it is higher or equal to my highest role.", color=discord.Color.red())
             await ctx.send(embed=embed)
             return  
 
         await member.add_roles(role)
 
-        embed = discord.Embed(color=discord.Color.green())
-        embed.add_field(name="User", value=member.mention, inline=False)
-        embed.add_field(name="Added Role", value=role.name, inline=False)
-        embed.set_footer(text=f"Executed by {ctx.author.display_name}")
+        embed = discord.Embed(description=f"<:success:1346853488738566175> {role.name} added to {member.mention} by {ctx.author.mention}", color=discord.Color.green())
         await ctx.send(embed=embed)
 
     # Command: Remove Role from User
@@ -53,29 +40,18 @@ class RoleAssignment(commands.Cog):
         role = discord.utils.get(guild.roles, name=role_name)
 
         if role is None:
-            embed = discord.Embed(
-                description=f"{ctx.author.mention} <:cancel:1346853536738316339> No role named `{role_name}` exists.",
-                color=discord.Color.red()
-            )
-            embed.set_footer(text="Ensure the role name is spelled correctly.")
+            embed = discord.Embed(description=f"{ctx.author.mention} <:cancel:1346853536738316339> No role named `{role_name}` exists.", color=discord.Color.red())
             await ctx.send(embed=embed)
             return
 
         if role not in member.roles:
-            embed = discord.Embed(
-                description=f"{ctx.author.mention} <:mention:1347449690849022092> {member.mention} does not have the `{role.name}` role.",
-                color=discord.Color.orange()
-            )
-            embed.set_footer(text="Ensure the role name is spelled correctly.")
+            embed = discord.Embed(description=f"{ctx.author.mention} <:mention:1347449690849022092> {member.mention} does not have the `{role.name}` role.", color=discord.Color.orange())
             await ctx.send(embed=embed)
             return
 
         await member.remove_roles(role)
 
-        embed = discord.Embed(color=discord.Color.green())
-        embed.add_field(name="User", value=member.mention, inline=False)
-        embed.add_field(name="Removed Role", value=role.name, inline=False)
-        embed.set_footer(text=f"Executed by {ctx.author.display_name}")
+        embed = discord.Embed(description=f"<:success:1346853488738566175> {role.name} removed from {member.mention} by {ctx.author.mention}", color=discord.Color.green())
         await ctx.send(embed=embed)
 
 async def setup(bot):
