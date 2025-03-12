d02db34a8acd3de4b4e1cbeceba9f1b944039e8c
spyromasked <159229360+spyromasked@users.noreply.github.com>
2025-03-13 00:01:04 +0530

    Create welcome.py

---
+++ cogs/welcome.py
@@ -0,0 +1,32 @@
+import discord
+from discord.ext import commands
+
+class Welcome(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        # Define the embed
+        embed = discord.Embed(
+            title=f"Welcome {member.mention} to the {member.guild.name}",
+            description="__**Navigate to the following links**__",
+            color=discord.Color.blue()
+        )
+
+        # Add server info, roles, and lounge links with clickable text
+        embed.add_field(name="Server Info", value="[Server Info](https://discord.com/channels/1260272793883508757/1349371140962914344)", inline=False)
+        embed.add_field(name="Roles", value="[Roles](https://discord.com/channels/1260272793883508757/1339188696221749269)", inline=False)
+        embed.add_field(name="Lounge", value="[Lounge](https://discord.com/channels/1260272793883508757/1339193406269685821)", inline=False)
+
+        # Add the GIF media link at the end
+        embed.set_image(url="https://cdn.discordapp.com/attachments/1339184630514974740/1349439380649148436/standard_10.gif?ex=67d31adb&is=67d1c95b&hm=16634f62e74ecb2b15bfba0094fa740a50618e801dfbd7b844f4ee82f68bc855&")
+
+        # Send the embed to the designated channel (replace with your desired channel ID)
+        channel = self.bot.get_channel(1260272793883508757)  # Replace with your desired channel ID
+        if channel:
+            await channel.send(embed=embed)
+
+# Setup function to add the cog to the bot
+async def setup(bot):
+    await bot.add_cog(Welcome(bot))
