b8e587f6c2dc3ce234ffbd37c85dece00b999a5a
spyromasked <159229360+spyromasked@users.noreply.github.com>
2025-03-13 00:16:43 +0530

    Update welcome.py

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -1,37 +1,33 @@
 import discord
 from discord.ext import commands
 
-class Welcome(commands.Cog):
+class WelcomeCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        # Print a log for debugging to check if the event is firing
-        print(f"{member} has joined the server.")
-        
-        # Define the embed
-        embed = discord.Embed(
-            title=f"Welcome {member.mention} to the {member.guild.name}",
-            description="__**Navigate to the following channel links**__",
-            color=discord.Color.blue()
-        )
+        welcome_channel = member.guild.get_channel(1349365300117635207)  # Welcome channel ID
 
-        # Add server info, roles, and lounge links with clickable text
-        embed.add_field(name="Server Info", value="[Server Info](https://discord.com/channels/1260272793883508757/1349371140962914344)", inline=False)
-        embed.add_field(name="Roles", value="[Roles](https://discord.com/channels/1260272793883508757/1339188696221749269)", inline=False)
-        embed.add_field(name="Lounge", value="[Lounge](https://discord.com/channels/1260272793883508757/1339193406269685821)", inline=False)
+        if welcome_channel:
+            embed = discord.Embed(
+                title=f"Welcome {member.mention} to the {member.guild.name}!",
+                color=discord.Color.blue()
+            )
+            
+            # Description
+            embed.description = "__**Navigate to the following channel links**__"
+            
+            # Add clickable links
+            embed.add_field(name="Server Info", value="[Click Here](https://discord.com/channels/1260272793883508757/1349371140962914344)", inline=False)
+            embed.add_field(name="Roles", value="[Click Here](https://discord.com/channels/1260272793883508757/1339188696221749269)", inline=False)
+            embed.add_field(name="Lounge", value="[Click Here](https://discord.com/channels/1260272793883508757/1339193406269685821)", inline=False)
+            
+            # Set GIF as the embed image
+            embed.set_image(url="https://cdn.discordapp.com/attachments/1339184630514974740/1349439380649148436/standard_10.gif?ex=67d31adb&is=67d1c95b&hm=16634f62e74ecb2b15bfba0094fa740a50618e801dfbd7b844f4ee82f68bc855&")
 
-        # Add the GIF media link at the end
-        embed.set_image(url="https://cdn.discordapp.com/attachments/1339184630514974740/1349439380649148436/standard_10.gif?ex=67d31adb&is=67d1c95b&hm=16634f62e74ecb2b15bfba0094fa740a50618e801dfbd7b844f4ee82f68bc855&")
+            await welcome_channel.send(embed=embed)
 
-        # Get the welcome channel (updated channel ID)
-        channel = self.bot.get_channel(1349365300117635207)  # Corrected welcome channel ID
-        if channel:
-            await channel.send(embed=embed)
-        else:
-            print("Couldn't find the channel to send the welcome message.")  # Error if channel not found
-
-# Setup function to add the cog to the bot
+# Add the cog to the bot
 async def setup(bot):
-    await bot.add_cog(Welcome(bot))
+    await bot.add_cog(WelcomeCog(bot))
