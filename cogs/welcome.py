8a0494d0fa33c0363345575a876fc42ac7276f4d
spyromasked <159229360+spyromasked@users.noreply.github.com>
2025-04-07 18:03:14 +0530

    Update welcome.py

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import asyncio
 
 class Welcome(commands.Cog):
     def __init__(self, bot):
@@ -8,6 +9,15 @@ class Welcome(commands.Cog):
     @commands.Cog.listener()
     async def on_member_join(self, member):
         channel = member.guild.get_channel(1339193406269685821)
+        role = member.guild.get_role(1349679460382806086)  # Auto assign role
+
+        # Safety check: make sure role exists
+        if role:
+            try:
+                await member.add_roles(role, reason="Auto role on member join")
+            except discord.Forbidden:
+                pass  # Bot may not have permission
+
         if channel is None:
             return
 
@@ -21,7 +31,9 @@ class Welcome(commands.Cog):
         view.add_item(discord.ui.Button(label="ℹ️ Server info", url="https://discord.com/channels/1260272793883508757/1337115224217751622", style=discord.ButtonStyle.link))
 
         message = await channel.send(content=member.mention, embed=embed, view=view)
-        await discord.utils.sleep_until(discord.utils.utcnow() + discord.timedelta(seconds=10))
+
+        await asyncio.sleep(10)
+
         try:
             await message.delete()
         except discord.NotFound:
