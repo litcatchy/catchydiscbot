8c23b1404ba67f995db3ceaf01959f8728b00691
spyromasked <159229360+spyromasked@users.noreply.github.com>
2025-04-09 14:33:28 +0530

    Update selfroles.py

--- cogs/selfroles.py
+++ cogs/selfroles.py
@@ -1,83 +1,152 @@
-import discord from discord.ext import commands from discord import ui, Interaction, Embed
+import discord
+from discord.ext import commands, tasks
 
-CHANNEL_ID = 1339188696221749269 GUILD_ID = 1260272793883508757
+class SelfRoles(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.panel_message = None
+        self.self_roles_channel_id = 1339188696221749269
+        self.gender_roles = {
+            "male": 1339877108347961386,
+            "female": 1339877064999960606
+        }
+        self.age_roles = {
+            "adult": 1339877455196061742,
+            "minor": 1339877409846988830
+        }
+        self.activity_roles = {
+            "edate": 1359438372245995600,
+            "polls": 1359438345679142932,
+            "vc": 1359438394429542480,
+            "chatreviver": 1359438442525884476
+        }
+        self.bot.loop.create_task(self.setup_panel())
 
-ROLE_IDS = { "male": 1339877108347961386, "female": 1339877064999960606, "adult": 1339877455196061742, "minor": 1339877409846988830, "edate": 1359438372245995600, "polls": 1359438345679142932, "vc": 1359438394429542480, "chat_reviver": 1359438442525884476, }
+    async def setup_panel(self):
+        await self.bot.wait_until_ready()
+        channel = self.bot.get_channel(self.self_roles_channel_id)
+        if channel is None:
+            print(f"Channel with ID {self.self_roles_channel_id} not found.")
+            return
 
-EMOJIS = { "male": "<:male:1359446626736803921>", "female": "<:female:1359446610391863316>", "adult": "<:adult:1359446704423702619>", "minor": "<:minor:1359446770966331392>", "edate": "<:edate:1359446814000156842>", "polls": "<:polls:1359446860229513256>", "vc": "📢", "chat_reviver": "<:chatreviver:1359438442525884476>", }
+        async for msg in channel.history(limit=10):
+            if msg.author == self.bot.user:
+                await msg.delete()
 
-current_panel_message = None
+        embed = discord.Embed(
+            title="Self Roles Panel",
+            description="Select your roles from the menus below!",
+            color=discord.Color.blurple()
+        )
 
-class SelfRoleView(ui.View): def init(self): super().init(timeout=None) self.add_item(GenderDropdown()) self.add_item(AgeDropdown()) self.add_item(ActivityDropdown())
+        view = SelfRoleView(self)
+        self.panel_message = await channel.send(embed=embed, view=view)
 
-class GenderDropdown(ui.Select): def init(self): options = [ discord.SelectOption(label="Female", value="female", emoji=EMOJIS["female"]), discord.SelectOption(label="Male", value="male", emoji=EMOJIS["male"]), ] super().init(placeholder=",gender", min_values=1, max_values=1, options=options)
+    async def assign_role(self, interaction: discord.Interaction, role: discord.Role):
+        if role in interaction.user.roles:
+            await interaction.user.remove_roles(role)
+            await interaction.response.send_message(
+                f"Removed role: `{role.name}`", ephemeral=True
+            )
+        else:
+            await interaction.user.add_roles(role)
+            await interaction.response.send_message(
+                f"Assigned role: `{role.name}`", ephemeral=True
+            )
 
-async def callback(self, interaction: Interaction):
-    await handle_role_selection(interaction, self.values[0], category="gender")
+class SelfRoleView(discord.ui.View):
+    def __init__(self, cog):
+        super().__init__(timeout=None)
+        self.cog = cog
+        self.add_item(GenderSelect(cog))
+        self.add_item(AgeSelect(cog))
+        self.add_item(ActivitySelect(cog))
 
-class AgeDropdown(ui.Select): def init(self): options = [ discord.SelectOption(label="Adult", value="adult", emoji=EMOJIS["adult"]), discord.SelectOption(label="Minor", value="minor", emoji=EMOJIS["minor"]), ] super().init(placeholder=",age", min_values=1, max_values=1, options=options)
+class GenderSelect(discord.ui.Select):
+    def __init__(self, cog):
+        self.cog = cog
+        options = [
+            discord.SelectOption(label="Female", value="female", emoji="<:female:1359446610391863316>"),
+            discord.SelectOption(label="Male", value="male", emoji="<:male:1359446626736803921>")
+        ]
+        super().__init__(placeholder="，gender", min_values=1, max_values=1, options=options)
 
-async def callback(self, interaction: Interaction):
-    await handle_role_selection(interaction, self.values[0], category="age")
+    async def callback(self, interaction: discord.Interaction):
+        role_id = self.cog.gender_roles[self.values[0]]
+        role = interaction.guild.get_role(role_id)
 
-class ActivityDropdown(ui.Select): def init(self): options = [ discord.SelectOption(label="E-Date", value="edate", emoji=EMOJIS["edate"]), discord.SelectOption(label="Polls", value="polls", emoji=EMOJIS["polls"]), discord.SelectOption(label="VC", value="vc", emoji=EMOJIS["vc"]), discord.SelectOption(label="Chat Reviver", value="chat_reviver", emoji=EMOJIS["chat_reviver"]), ] super().init(placeholder=",activity", min_values=1, max_values=len(options), options=options)
+        # Remove other gender role if present
+        for gender_role_id in self.cog.gender_roles.values():
+            r = interaction.guild.get_role(gender_role_id)
+            if r in interaction.user.roles and r != role:
+                await interaction.user.remove_roles(r)
 
-async def callback(self, interaction: Interaction):
-    for value in self.values:
-        await handle_role_selection(interaction, value, category="activity", ephemeral=False)
-    await interaction.response.send_message("Your selected roles have been updated.", ephemeral=True)
+        await self.cog.assign_role(interaction, role)
 
-async def handle_role_selection(interaction: Interaction, selected_value: str, category: str = None, ephemeral: bool = True): member = interaction.user guild = interaction.guild role = guild.get_role(ROLE_IDS[selected_value])
+class AgeSelect(discord.ui.Select):
+    def __init__(self, cog):
+        self.cog = cog
+        options = [
+            discord.SelectOption(label="Adult", value="adult", emoji="<:adult:1359446704423702619>"),
+            discord.SelectOption(label="Minor", value="minor", emoji="<:minor:1359446770966331392>")
+        ]
+        super().__init__(placeholder="，age", min_values=1, max_values=1, options=options)
 
-if category in ["gender", "age"]:
-    category_roles = {
-        k: v for k, v in ROLE_IDS.items() if (category == "gender" and k in ("male", "female")) or (category == "age" and k in ("adult", "minor"))
-    }
-    for role_name, role_id in category_roles.items():
-        existing_role = guild.get_role(role_id)
-        if existing_role in member.roles and existing_role != role:
-            await member.remove_roles(existing_role)
+    async def callback(self, interaction: discord.Interaction):
+        role_id = self.cog.age_roles[self.values[0]]
+        role = interaction.guild.get_role(role_id)
 
-if role in member.roles:
-    await member.remove_roles(role)
-    response = f"[`{role.name}`] has been successfully removed."
-else:
-    await member.add_roles(role)
-    response = f"[`{role.name}`] has been assigned."
+        # Remove other age role if present
+        for age_role_id in self.cog.age_roles.values():
+            r = interaction.guild.get_role(age_role_id)
+            if r in interaction.user.roles and r != role:
+                await interaction.user.remove_roles(r)
 
-if ephemeral:
-    await interaction.response.send_message(response, ephemeral=True)
-else:
-    await interaction.followup.send(response, ephemeral=True)
+        await self.cog.assign_role(interaction, role)
 
-class SelfRoles(commands.Cog): def init(self, bot): self.bot = bot self.panel_task.start()
+class ActivitySelect(discord.ui.Select):
+    def __init__(self, cog):
+        self.cog = cog
+        options = [
+            discord.SelectOption(label="Edate", value="edate", emoji="<:edate:1359446814000156842>"),
+            discord.SelectOption(label="Polls", value="polls", emoji="<:polls:1359446860229513256>"),
+            discord.SelectOption(label="VC", value="vc", emoji="📢"),
+            discord.SelectOption(label="Chat Reviver", value="chatreviver", emoji="<:chatreviver:1359448891983921272>")
+        ]
+        super().__init__(placeholder="，activity", min_values=1, max_values=len(options), options=options)
 
-def cog_unload(self):
-    self.panel_task.cancel()
+    async def callback(self, interaction: discord.Interaction):
+        roles_to_add = []
+        roles_to_remove = []
 
-@tasks.loop(count=1)
-async def panel_task(self):
-    global current_panel_message
-    await self.bot.wait_until_ready()
-    channel = self.bot.get_channel(CHANNEL_ID)
-    if not channel:
-        return
+        selected_role_ids = {self.cog.activity_roles[v] for v in self.values}
+        user_role_ids = {role.id for role in interaction.user.roles}
 
-    try:
-        history = [message async for message in channel.history(limit=10)]
-        for message in history:
-            if message.author == self.bot.user and message.components:
-                await message.delete()
-    except Exception:
-        pass
+        for value, role_id in self.cog.activity_roles.items():
+            role = interaction.guild.get_role(role_id)
+            if role_id in selected_role_ids:
+                if role_id not in user_role_ids:
+                    roles_to_add.append(role)
+            else:
+                if role_id in user_role_ids:
+                    roles_to_remove.append(role)
 
-    embed = Embed(title="Self Roles Panel", description="Select your roles below:", color=discord.Color.blurple())
-    embed.add_field(name="Gender", value=f"{EMOJIS['female']} Female\n{EMOJIS['male']} Male", inline=False)
-    embed.add_field(name="Age", value=f"{EMOJIS['adult']} Adult\n{EMOJIS['minor']} Minor", inline=False)
-    embed.add_field(name="Activity", value=f"{EMOJIS['edate']} E-Date\n{EMOJIS['polls']} Polls\n{EMOJIS['vc']} VC\n{EMOJIS['chat_reviver']} Chat Reviver", inline=False)
-    embed.set_footer(text="Select your roles from the dropdowns below!")
+        if roles_to_add:
+            await interaction.user.add_roles(*roles_to_add)
 
-    current_panel_message = await channel.send(embed=embed, view=SelfRoleView())
+        if roles_to_remove:
+            await interaction.user.remove_roles(*roles_to_remove)
 
-async def setup(bot): await bot.add_cog(SelfRoles(bot))
+        messages = []
+        for role in roles_to_add:
+            messages.append(f"Assigned role: `{role.name}`")
+        for role in roles_to_remove:
+            messages.append(f"Removed role: `{role.name}`")
 
+        if not messages:
+            messages.append("No changes were made.")
+
+        await interaction.response.send_message("\n".join(messages), ephemeral=True)
+
+async def setup(bot):
+    await bot.add_cog(SelfRoles(bot))
