a19174219d4696cae7bb83dbb6b39eec98f65196
catchy <159229360+spyromasked@users.noreply.github.com>
2025-04-27 17:32:58 +0530

    Update boost.py

--- cogs/boost.py
+++ cogs/boost.py
@@ -11,26 +11,53 @@ class Boosts(commands.Cog):
     async def rb(self, ctx):
         """Command to display the top boosters."""
         channel = ctx.channel
+        # Mention the user who boosted and thank them
+        user = ctx.author
+        await self.send_thank_you_message(user, channel)
+        # Send the top boosters message after thanking
         await self.send_top_boosters(channel)
+
+    async def send_thank_you_message(self, user, channel):
+        """Sends a thank you message to the user who boosted."""
+        thank_you_embed = discord.Embed(
+            title="Thank You for Boosting!",
+            description=f"<@{user.id}> thank you so much for boosting bby! We love you so much. <:000:1358756048982249574>\n\n"
+                        f"-# Perks included: hoisted role, cute icon & image perms!",
+            color=discord.Color.greyple()  # Grey color for the embed
+        )
+        await channel.send(embed=thank_you_embed)
 
     async def send_top_boosters(self, channel):
         """Sends the top boosters to the channel."""
         boosters = await self.fetch_recent_boosters(channel)
         
         if not boosters:
-            await channel.send("No boosters found in the last 14 days.")
+            embed = discord.Embed(
+                title="No Boosters Found",
+                description="No boosters found in the last 14 days.",
+                color=discord.Color.greyple()  # Grey color for the embed
+            )
+            await channel.send(embed=embed)
             return
 
         # Sort boosters by the number of boosts (assuming boosters is a list of users)
-        # You can add more complex sorting here if necessary
         sorted_boosters = sorted(boosters, key=lambda user: user['boost_count'], reverse=True)
         
-        # Prepare the message to send top boosters
-        message = "Top Boosters in the last 14 days:\n"
-        for index, booster in enumerate(sorted_boosters[:5], start=1):
-            message += f"{index}. {booster['user'].name} - Boosts: {booster['boost_count']}\n"
+        # Prepare the message to send top boosters in embed
+        embed = discord.Embed(
+            title="Top Boosters in the Last 14 Days",
+            description="Here are the top boosters in the last 14 days:",
+            color=discord.Color.greyple()  # Grey color for the embed
+        )
         
-        await channel.send(message)
+        for index, booster in enumerate(sorted_boosters[:5], start=1):
+            embed.add_field(
+                name=f"{index}. {booster['user'].name}",
+                value=f"Boosts: {booster['boost_count']}",
+                inline=False
+            )
+        
+        await channel.send(embed=embed)
 
     async def fetch_recent_boosters(self, channel):
         """Fetches the users who boosted in the last 14 days."""
