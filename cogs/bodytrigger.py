efcdc5ba0ebd8a91719717351e5e2ffe43bfd8ae
catchy <159229360+spyromasked@users.noreply.github.com>
2025-05-11 17:01:57 +0530

    Update bodytrigger.py

--- cogs/bodytrigger.py
+++ cogs/bodytrigger.py
@@ -3,28 +3,72 @@ from discord.ext import commands
 import aiohttp
 import re
 import asyncio
-import random
 from bs4 import BeautifulSoup
+import random
 
 class PFPTrigger(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.sent_images = set()  # To keep track of already sent images
-
         self.triggers = {
-            r"\blips?\b": ["sexy female lips", "sensual lips close-up"],
-            r"\beyes?\b": ["seductive female eyes", "intense eye contact woman"],
-            r"\bboobs?\b": ["hot boobs aesthetic", "sensual bust female", "boobs in lingerie"],
-            r"\bwaists?\b": ["sexy slim waist", "lingerie waist curve"],
-            r"\babs?\b": ["sexy female abs", "toned abs in lingerie"],
-            r"\bass(es)?\b": ["big ass aesthetic", "sexy booty in panties"],
-            r"\bbiceps?\b": ["muscular biceps flexing", "veiny arms male fitness"],
-            r"\bveins?\b": ["veiny arms aesthetic", "close up veiny muscles"],
-            r"\bhunter eyes?\b": ["intense hunter eyes male", "dominant male stare"],
-            r"\bhands?\b": ["female hands on body", "hands in sensual pose"],
-            r"\bchoke\b": ["aesthetic choking kink", "dominant hand on neck"],
-            r"\bbathe?\b": ["nude bath aesthetic", "sensual bath photo"],
-            r"\bbaths?\b": ["woman in bathtub aesthetic", "lingerie bath shoot"]
+            r"\blips?\b": [
+                "aesthetic female lips site:pinterest.com",
+                "sensual female lips site:pinterest.com"
+            ],
+            r"\beyes?\b": [
+                "aesthetic female eyes site:pinterest.com",
+                "seductive eyes woman site:pinterest.com"
+            ],
+            r"\bboobs?\b": [
+                "boobs aesthetic site:pinterest.com",
+                "aesthetic boobs site:pinterest.com",
+                "cleavage aesthetic site:pinterest.com"
+            ],
+            r"\bwaists?\b": [
+                "women aesthetic waist site:pinterest.com",
+                "sexy women waist site:pinterest.com",
+                "tight waist aesthetic site:pinterest.com"
+            ],
+            r"\babs?\b": [
+                "female aesthetic abs site:pinterest.com",
+                "male aesthetic abs site:pinterest.com",
+                "sexy abs woman site:pinterest.com"
+            ],
+            r"\bass(es)?\b": [
+                "female aesthetic ass site:pinterest.com",
+                "tight ass aesthetic site:pinterest.com",
+                "sexy woman ass site:pinterest.com"
+            ],
+            r"\bbiceps?\b": [
+                "male aesthetic veiny biceps site:pinterest.com",
+                "shirtless veiny biceps site:pinterest.com",
+                "flexed biceps aesthetic site:pinterest.com"
+            ],
+            r"\bveins?\b": [
+                "veiny arms aesthetic site:pinterest.com",
+                "aesthetic veiny arms site:pinterest.com",
+                "veiny muscular arms site:pinterest.com"
+            ],
+            r"\bhunter eyes?\b": [
+                "male aesthetic hunter eyes site:pinterest.com",
+                "intense male gaze site:pinterest.com"
+            ],
+            r"\bhands?\b": [
+                "female aesthetic hands site:pinterest.com",
+                "sensual hands site:pinterest.com"
+            ],
+            r"\bchoke\b": [
+                "aesthetic choke site:pinterest.com",
+                "aesthetic female getting choked site:pinterest.com",
+                "choke kink aesthetic site:pinterest.com"
+            ],
+            r"\bbathe?\b": [
+                "aesthetic girl bath site:pinterest.com",
+                "nude aesthetic bath site:pinterest.com"
+            ],
+            r"\bbaths?\b": [
+                "aesthetic girl bath site:pinterest.com",
+                "hot bath woman aesthetic site:pinterest.com"
+            ]
         }
 
     @commands.Cog.listener()
@@ -36,16 +80,10 @@ class PFPTrigger(commands.Cog):
             if re.search(pattern, message.content, re.IGNORECASE):
                 query = random.choice(queries)
                 img_url = await self.fetch_image(query)
-                if img_url and img_url not in self.sent_images:
-                    try:
-                        sent_msg = await message.channel.send(img_url)
-                        await asyncio.sleep(3.5)  # Wait for 3.5 seconds before deleting the message
-                        await sent_msg.delete()
-                        self.sent_images.add(img_url)
-                        if len(self.sent_images) > 10:
-                            self.sent_images.pop()
-                    except discord.HTTPException as e:
-                        print(f"Failed to send or delete message: {e}")
+                if img_url:
+                    sent_msg = await message.channel.send(img_url)
+                    await asyncio.sleep(3.5)
+                    await sent_msg.delete()
                 break
 
     async def fetch_image(self, query):
@@ -53,24 +91,21 @@ class PFPTrigger(commands.Cog):
         search_url = f"https://www.bing.com/images/search?q={query.replace(' ', '+')}&form=HDRSC2"
 
         async with aiohttp.ClientSession() as session:
-            try:
-                async with session.get(search_url, headers=headers) as response:
-                    if response.status != 200:
-                        print(f"Failed to fetch images for query: {query}")
-                        return None
-                    html = await response.text()
-                    soup = BeautifulSoup(html, "html.parser")
-                    image_elements = soup.select("a.iusc")
-                    for tag in image_elements:
-                        m = tag.get("m")
-                        if m and '"murl":"' in m:
-                            start = m.find('"murl":"') + len('"murl":"')
-                            end = m.find('"', start)
-                            image_url = m[start:end].replace("\\", "")
-                            if any(ext in image_url for ext in [".jpg", ".jpeg", ".png"]):
-                                return image_url
-            except aiohttp.ClientError as e:
-                print(f"HTTP request failed: {e}")
+            async with session.get(search_url, headers=headers) as response:
+                if response.status != 200:
+                    return None
+                html = await response.text()
+                soup = BeautifulSoup(html, "html.parser")
+                image_elements = soup.select("a.iusc")
+
+                for tag in image_elements:
+                    m = tag.get("m")
+                    if m and '"murl":"' in m:
+                        start = m.find('"murl":"') + len('"murl":"')
+                        end = m.find('"', start)
+                        image_url = m[start:end].replace("\\", "")
+                        if image_url.endswith(('.jpg', '.jpeg', '.png')) and "pinterest" in image_url:
+                            return image_url
         return None
 
 async def setup(bot):
